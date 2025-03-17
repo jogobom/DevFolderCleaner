8813318b96aa0dfcd74a7cb7fe4e53f43378238b
Chris Preston <chris_preston@waters.com>
2025-03-17 18:11:31 +0000

    cleanBuilds command
    And a bunch of CI stuff

---
+++ CleanBuildFoldersCommand.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace DevFolderCleaner;
+
+public class CleanBuildFoldersCommand : Command<CleanBuildFoldersCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        [Description("Path to search for  build folders.")]
+        [CommandArgument(0, "<searchPath>")]
+        public required string SearchPath { get; init; }
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        var foldersToDelete =
+            Directory.EnumerateDirectories(settings.SearchPath, "bin", SearchOption.AllDirectories).Concat(
+                    Directory.EnumerateDirectories(settings.SearchPath, "obj", SearchOption.AllDirectories))
+                .Where(f => !f.Contains("node_modules"))
+                .ToList();
+
+        AnsiConsole.MarkupLine("I'm going to [bold]delete[/] the following folders:");
+
+        foreach (var folder in foldersToDelete)
+        {
+            Console.WriteLine(folder);
+        }
+
+        const string confirmCode = "order 66";
+        AnsiConsole.MarkupLine($"[green]Say [bold]'{confirmCode}'[/] to confirm:[/]");
+
+        if (Console.ReadLine() != confirmCode)
+        {
+            AnsiConsole.MarkupLine("[green]Those folders will not die this day.[/]");
+            return 0;
+        }
+
+        foreach (var folder in foldersToDelete)
+        {
+            try
+            {
+                Directory.Delete(folder, true);
+                AnsiConsole.MarkupLine($"Deleted {folder}");
+            }
+            catch (Exception)
+            {
+                AnsiConsole.MarkupLine($"[red]Unable to delete {folder}[/]");
+                throw;
+            }
+        }
+        return 0;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,23 @@
 ﻿using DevFolderCleaner;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
-var app = new CommandApp<DeleteOrphanedFoldersCommand>();
-return await app.RunAsync(args);
+var app = new CommandApp();
+app.Configure(config =>
+{
+#if DEBUG
+    config.PropagateExceptions();
+    config.ValidateExamples();
+#endif
+    config.AddCommand<DeleteOrphanedFoldersCommand>("deleteOrphans");
+    config.AddCommand<CleanBuildFoldersCommand>("cleanBuilds").WithExample("cleanBuilds", "C:/code");
+});
+try
+{
+    return await app.RunAsync(args);
+}
+catch (Exception ex)
+{
+    AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+    return -1;
+}
